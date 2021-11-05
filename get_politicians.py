8afa6f84bf16a61e3f2b7116090ed249ca5da45c
olid901 <63404298+olid901@users.noreply.github.com>
2021-11-05 22:17:15 +0100

    fixed Script so it finds all Politicians

--- get_politicians.py
+++ get_politicians.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     # Database setup
     connection = sqlite3.connect("politicians.db")
     cursor = connection.cursor()
+    cursor.execute("DROP TABLE politician")
     create_politiocian_table = "CREATE TABLE IF NOT EXISTS politician (" \
                                "id INTEGER UNIQUE PRIMARY KEY," \
                                "first_name STRING," \
@@ -18,20 +19,32 @@ if __name__ == '__main__':
     cursor.execute(create_politiocian_table)
 
     # Es gibt 30k Politker auf Abgeordnetenwatch, daher range(31) (geht von 0 bis 30)
-    for cnt in range(31):
-        response = requests.get("https://www.abgeordnetenwatch.de/api/v2/politicians?range_start=%d&range_end=%d"%(cnt*1000, cnt*1000 + 1000))
+    for page in range(31):
+        url = f"https://www.abgeordnetenwatch.de/api/v2/politicians?page=%d&pager_limit=1000" % page
+        print(url)
+        response = requests.get(url)
         json_data = json.loads(response.content.decode("ASCII"))
+        print(len(json_data['data']))
         for politician in json_data['data']:
-            #Format insert Command with id, first name, last name, birth name, sex, party
+
+            # Schlechte Daten rausfiltern: Es gibt eine Politiker, der überall außer bei der Partei null als Wert hat
+            if politician['label'] is None or politician['label'] == "" or politician['label'] == " ":
+                continue
+
+            # Format insert Command with id, first name, last name, birth name, sex, party
             insert_command = 'INSERT INTO politician VALUES ({}, \'{}\', \'{}\', {}, \'{}\', {}, {})'.format(
-            politician['id'],
-            politician['first_name'].replace("'", "''"), #mögl. Apostroph im Namen durch "''" ersetzen, damit der Befehl SQL-konform ist
-            politician['last_name'].replace("'", "''"),
-            "'"+politician['birth_name'].replace("'", "''")+"'" if politician['birth_name'] is not None else 'null',
-            politician['sex'], politician['year_of_birth'] if politician['year_of_birth'] is not None else "null",
-            politician['party']['id'] if politician['party'] is not None else "null")
+                politician['id'],
+                # mögl. Apostroph im Namen durch "''" ersetzen, damit der Befehl SQL-konform ist
+                politician['first_name'].replace("'", "''"),
+                politician['last_name'].replace("'", "''"),
+                "'"+politician['birth_name'].replace("'", "''")+"'" if politician['birth_name'] is not None else 'null',
+                politician['sex'], politician['year_of_birth'] if politician['year_of_birth'] is not None else "null",
+                politician['party']['id'] if politician['party'] is not None else "null")
             cursor.execute(insert_command)
 
-    cursor.execute("SELECT * FROM politician")
+    #Kurz gucken ob die Größe der Tabelle stimmt, sollten ~29k sein
+    cursor.execute("SELECT COUNT(*) FROM politician")
     all = cursor.fetchall()
     print(all)
+    # Changes an der Datenbank commiten, damit sie gespeichert werden
+    connection.commit()
