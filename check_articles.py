b3fffacaecb7df6dbcc442bf0e2fbb22cd1ae5df
olid901 <63404298+olid901@users.noreply.github.com>
2021-12-01 19:40:25 +0100

    bessere Politiker Erkennung, politicians.db und articles.db zusammengeführt

--- check_articles.py
+++ check_articles.py
@@ -1,37 +1,72 @@
 import sqlite3
+import requests
+import json
 
-# Handy python script to check  if there is a wikipedia article for a politician 
+# Handy python script to check  if there is a wikipedia article for a politician
+
+cnt = 0
+
+def count_in_list(list, item):
+    global cnt
+    cnt += 1
+    if cnt%10 == 0:
+        print(cnt)
+
+    count = 0
+    for x in list:
+        if x == item:
+            count += 1
+    return count
+
 def main():
     connection = sqlite3.connect("politicians.db")
     cursor = connection.cursor()
     
     # We already have (almost) all politicians in our politicians database
     # In this case, we only need the first and last name
-    cursor.execute("SELECT DISTINCT first_name, last_name FROM politicians ORDER BY first_name, last_name ASC")
+    cursor.execute("SELECT DISTINCT id, first_name, last_name FROM politicians ORDER BY first_name, last_name ASC")
     res = cursor.fetchall()
 
-    politicians = set()
+    politicians = dict()
 
-    for first, last in res:
-        name = first + ' ' + last
-        name = name.replace(' ', '_')
-        politicians.add(name)
-    
+    for id, first, last in res:
+        politicians[id] = str(first + ' ' + last).replace(' ', '_')
+
     # We use a list of all wikipedia articles, which can be downloaded here:
     # https://ftp.acc.umu.se/mirror/wikimedia.org/dumps/dewiki/20211001/dewiki-20211001-all-titles-in-ns0.gz
     my_file = open("files/dewiki-20211001-all-titles-in-ns0", "rt", encoding='utf-8')
     list_file = my_file.read().splitlines()
     articles = set(list_file)
 
-    matches = politicians.intersection(articles)
+    matches = {int(key):val for key, val in politicians.items() if val in articles}
 
     print("Politicians: " + str(len(politicians)))
     print("Matches: " + str(len(matches)))
-    
-    file = open("article_list.txt", "w", encoding="utf-8")
-    for name in matches:
-        file.write(name + "\n")
-    file.close()
+
+    # in rev_multidict werden alle Einträge gespeichert, deren values (Vor und Nachname) in Matches mehrmals vorkommt
+    # StackOverflow sei Dank!
+    rev_multidict = {}
+    for key, value in matches.items():
+        rev_multidict.setdefault(value, set()).add(key)
+    dup_list = [value for key, value in rev_multidict.items() if len(value) > 1]
+
+    # Problem: There are politicians with the same Name but (obviously) different IDs.
+    # It cant be determined which politician the wikipedia page refers to, there it's impossible to know which politicianID to map to the article.
+    # So it is better to remove them and have consistent Data, where every wikipedia page refers to one politicianID
+    for id_set in dup_list:
+        for id in id_set:
+            if id in matches:
+                del matches[id]
+
+    print("Politicians after duplicate removal: ", len(matches))
+
+    # Flip key and values: We need the Name as key, as we will never search this json by a politicianID, but in by name
+    flipped = dict((v,k) for k,v in matches.items())
+
+    with open("article_list.json", "w", encoding="utf-8") as json_file:
+        json.dump(flipped, json_file)
+
 
 if __name__ == '__main__':
+
     main()
--- wikipedia.py
+++ wikipedia.py
@@ -10,6 +10,7 @@ from time import time, sleep
 from multiprocessing import Process, JoinableQueue
 from os import listdir
 from os.path import isfile, join
+import json
 
 dump_url = "https://dumps.wikimedia.org/dewiki/20211101/"
 base_url = "https://dumps.wikimedia.org"
@@ -38,16 +39,18 @@ class Revision:
 
 class HistoryPage:
 
-    def __init__(self, page_id, title, revisions, text):
+    def __init__(self, page_id, title, politician_id, revisions, text):
         self.text = text
         self.revisions = revisions
         self.title = title
         self.id = page_id
+        self.politician_id = politician_id
 
     def __str__(self):
         s = f" \
         title: {self.title}\n \
         id: {self.id}\n \
+        politician_id: {self.politician_id}\n \
         revisions: \n"
 
         for r in self.revisions:
@@ -73,7 +76,7 @@ class HistoryPage:
 
 class DatabaseHandler:
     def __init__(self):
-        self._connection = sqlite3.connect("articles.db", timeout=5)
+        self._connection = sqlite3.connect("politicians.db", timeout=5)
         self._cursor = self._connection.cursor()
 
     def commit(self):
@@ -84,10 +87,11 @@ class DatabaseHandler:
         self._cursor.execute("DROP TABLE IF EXISTS Page")
         self._cursor.execute("DROP TABLE IF EXISTS Revision")
         self._cursor.execute("CREATE TABLE IF NOT EXISTS Contributor (id INTEGER PRIMARY KEY, name STRING)")
-        self._cursor.execute("INSERT INTO Contributor VALUES(-1, null)")
         self._cursor.execute("CREATE TABLE IF NOT EXISTS Page ("
                              "id INTEGER UNIQUE PRIMARY KEY,"
-                             "title STRING)")
+                             "title STRING,"
+                             "politician_id INTEGER,"
+                             "FOREIGN KEY (politician_id) REFERENCES politicians(id))")
         self._cursor.execute("CREATE TABLE IF NOT EXISTS Revision ("
                              "id INTEGER UNIQUE PRIMARY KEY,"
                              "timestamp TIMESTAMP,"
@@ -97,29 +101,28 @@ class DatabaseHandler:
                              "FOREIGN KEY (contributor_id) REFERENCES Contributor(id),"
                              "FOREIGN KEY (page_id) REFERENCES Page(id))")
 
+    # !!! Can return None when contributor only had an ip-Address in xml and no username or id
     @staticmethod
     def insert_contributor_command(contributor: Contributor):
         if contributor.id is not None and contributor.name is not None:
-            return f"""INSERT OR IGNORE INTO Contributor VALUES({contributor.id}, '{contributor.name.replace("'", "''") if contributor.name else "null"}')"""
+            # replace: escape "'" in SQL-Query
+            return f"""INSERT OR IGNORE INTO Contributor VALUES({contributor.id}, '{contributor.name.replace("'", "''")}')"""
         else:
             return None
 
     @staticmethod
     def insert_page_command(page: HistoryPage):
-        # print("Inserting Page: " + page.title)
-        return f"INSERT INTO Page VALUES({page.id}, '{page.title}')"
+        return f"INSERT INTO Page VALUES({page.id}, '{page.title}', {page.politician_id})"
 
     @staticmethod
     def insert_revision_command(revision: Revision):
-        # print("inserting Revision: ", revision.id)
         return f"INSERT OR IGNORE INTO Revision (timestamp, text_len, contributor_id, page_id) VALUES(" \
                f" '{revision.timestamp}'," \
                f" {revision.text_len}," \
-               f"{revision.contributor.id if revision.contributor.id else -1}," \
+               f"{revision.contributor.id if revision.contributor.id else 'null'}," \
                f" {revision.page_id})"
 
     def execute_command(self, command):
-        # print("executing command: ", command)
         self._cursor.execute(command)
 
 
@@ -140,29 +143,37 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
         # current tag
         self._current_tag = None
 
+        # buffers for page and contributor
         self._contributor_object_buffer = None
-        self._revisions_object_buffer = []
-        self._latest_text = None
-        # actual public Data
-        self.HistoryPages = []
+        self._HistoryPage_object_buffer = None
 
+        # Buffer List for all Revisions of a page
+        self._revisions_object_buffer = []
+        # We don't save the text of the revisions to the db, so we don't save them to buffer objects either. But we
+        # need to keep track of the text from text last processed revision, as we save that to the page.
+        self._latest_text = None
+
+        # Queue to put sql Queries in
         self._sql_queue = sql_queue
 
+        # Page is surely not a politician: don't write anything to buffers, as we don't save anything about the page to the db.
+        # Performance-Boost: ~25%
         self._performance_skip = False
 
-        # There are noe Thread/Process-safe global variables in python, so every parser needs to know this
-        my_file = open("article_list.txt", "rt", encoding='utf-8')
-        self._politician_list = my_file.read().splitlines()
+        # Afaik there are no Thread/Process-safe global variables in python, so every parser needs to know this
+        with open('article_list.json') as json_file:
+            self._politician_dict = json.load(json_file)
 
     def characters(self, content):
         """Characters between opening and closing tags"""
 
         # I AM SPEED: If an article is surely not a politician (does NOT imply it is a politician!), dont write anything to buffers
         if self._in_tag == "page" and self._current_tag == "title" and content.strip().replace("\n", "") != "":
-            if content.replace(" ", "_").strip() not in self._politician_list:
+            if content.replace(" ", "_").strip() not in self._politician_dict:
                 print("This is not a politician: ", content)
                 self._performance_skip = True
             else:
+                print("This could be a politician: ", content)
                 self._performance_skip = False
 
         # We can skip the page if the title doesnt fit to any politician
@@ -183,7 +194,6 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
 
     def startElement(self, name, attrs):
         """Opening tag of element"""
-
         if name == "page":
             self._page_buffer = []
             self._in_tag = "page"
@@ -227,7 +237,6 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
                                                                          "") if 'id' in self._contributor_values else None
                 )
                 self._contributor_values = {}
-                # DB_handler.insert_contributor(self._contributor_object_buffer)
 
             if name == 'revision':
                 self._in_tag = "page"
@@ -243,21 +252,23 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
 
             if name == 'page':
                 self._in_tag = None
-                self.HistoryPages.append(
-                    HistoryPage(self._page_values['id'], self._page_values['title'], self._revisions_object_buffer,
-                                self._latest_text)
-                )
+                self._HistoryPage_object_buffer = HistoryPage(self._page_values['id'], self._page_values['title'],
+                                                              self._politician_dict[
+                                                           self._page_values['title'].replace(" ", "_")],
+                                                              self._revisions_object_buffer,
+                                                              self._latest_text)
+
                 self._page_values = {}
 
-                if is_politician(self.HistoryPages[-1]):
+                if is_politician(self._HistoryPage_object_buffer):
                     print("Found a politician!")
-                    print(self.HistoryPages[-1].title)
-                    for rev in self.HistoryPages[-1].revisions:
+                    print(self._HistoryPage_object_buffer.title)
+                    for rev in self._HistoryPage_object_buffer.revisions:
                         self._sql_queue.put(DatabaseHandler.insert_contributor_command(rev.contributor))
                         self._sql_queue.put(DatabaseHandler.insert_revision_command(rev))
-                    self._sql_queue.put(DatabaseHandler.insert_page_command(self.HistoryPages[-1]))
+                    self._sql_queue.put(DatabaseHandler.insert_page_command(self._HistoryPage_object_buffer))
                 else:
-                    print("No politician (", self.HistoryPages[-1].title, ")")
+                    print("No politician (", self._HistoryPage_object_buffer.title, ")")
 
 
 # Worker process method for concurrent XML parsing
@@ -289,41 +300,28 @@ def end_program_when_done(file_queue: JoinableQueue, sql_queue: Queue, db_handle
     file_queue.join()
 
 
-
-# TODO: Make this more failsafe, maybe by looking at the text: Does ist contain or Link "Politiker"
+# We check if an article belongs to a politician in two ways.
+# 1. The title of the page must match the name of a politician from the database
+# 2. The Page must link [[Politiker]] somewhere in the text
+# The 1st step is already done in the parser. The 2nd step is here:
 def is_politician(page: HistoryPage):
-    my_file = open("article_list.txt", "rt", encoding='utf-8')
-    politician_list = my_file.read().splitlines()
-    if page.title.replace(" ", "_").strip() in politician_list:
+    if "[[Politiker]]" in page.text:
+        print("This is a politician: ", page.title)
         return True
-    else:
-        return False
-
-
-# Code to get all .bz2 files
-'''
-# Retrieve the html
-dump_html = requests.get(dump_url).text  # Convert to a soup
-soup_dump = BeautifulSoup(dump_html, 'html.parser')  # Find list elements with the class file
-bz2_files = [x.get('href') for x in soup_dump.find_all('a') if
-             "dewiki-20211101-pages-meta-history" in x.get('href') and x.get('href')[-4:] == ".bz2"]
-'''
 
 
 # According to SQLite3 docs, the library is capable of multiprocessing,
 # which means, we share it between all processes
-# TODO: Wäre es ggf. schöner, wenn wir nicht einfach so auf des Ding aus dem nichts zugreifen
-#       sondern den vlt. der Methode mit übergeben? -> Prüfen, ob das Probleme macht
 
 # Main Method to cover Scope of Variables in here
 def main():
-    DB_handler = DatabaseHandler()
-    DB_handler.create_database_tables()
+    db_handler = DatabaseHandler()
+    db_handler.create_database_tables()
 
     file_queue = JoinableQueue(maxsize=0)
     sql_queue = Queue(maxsize=0)
     # TODO: Change number of processes based on user dialog or command argument
-    num_threads = 4
+    num_threads = 1
 
     file_path = "./files/"
     file_list = ["./files/" + f for f in listdir(file_path) if isfile(join(file_path, f))]
@@ -332,37 +330,33 @@ def main():
         file_queue.put(file)
 
     # For Debugging
-    #file_queue.put("files/dewiki-20211001-pages-meta-history1.xml-p1p1598.bz2")
+    # file_queue.put("files/dewiki-20211001-pages-meta-history1.xml-p1p1598.bz2")
 
     for i in range(num_threads):
         worker = Process(target=dump_wikipedia_worker, args=(file_queue, sql_queue,))
         worker.start()
 
-    # This needs to be a separate process, as file_queue.join() would block main Thread and we need to execute the sql commands here
-    end_script_worker = Process(target=end_program_when_done, args=(file_queue, sql_queue, DB_handler))
+    # This needs to be a separate process, as file_queue.join() would block main Thread and we need to execute the
+    # sql commands here
+    end_script_worker = Process(target=end_program_when_done, args=(file_queue, sql_queue, db_handler))
     end_script_worker.start()
 
-    # Check for sql Queries ind Queue and execute
+    # Check for sql Queries in Queue and execute
     while True:
         if not sql_queue.empty():
             command = sql_queue.get(timeout=5)
             if command is not None:
-                DB_handler.execute_command(command)
+                db_handler.execute_command(command)
         else:
             if not end_script_worker.is_alive():
-                print("c...")
-                DB_handler.commit()
-                print("c!")
-                exit(1)
-
-    # TODO: This part is broken currently, find out how to fix or delete
-    # for x in handler.HistoryPages:
-    #     if x.title in politician_list or x.title.replace(" ", "_") in politician_list or x.title.replace(" ", "_").strip() in politician_list or x.title.replace("(Politiker)","").replace(" ", "_").strip() in politician_list:
-    #         print(x.title)
+                print("committing...")
+                db_handler.commit()
+                print("committed!")
+                return
 
 
 if __name__ == "__main__":
     start = time()
     main()
     finish = time()
-    print("Duration:"+str((finish - start)//60)+":"+str((start-finish)%60)+"min")
+    print("Duration: " + str((finish - start) // 3600) + ":" + str((finish - start) // 60) + ":" + str((start - finish) % 60) + "s")
